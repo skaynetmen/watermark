20fff84d3378195488d5bdfd42616e26f64866af
Vadim Novash <novash@tut.by>
2016-01-14 13:08:25 +0300

    some changes without result

--- app/js/modules/fileUpload.js
+++ app/js/modules/fileUpload.js
@@ -15,25 +15,42 @@ var _fileUploadFn = function (inputFile, container) {
 
 	//рабочие элементы
 	var inputImg = $(inputFile),
-			inputImgContainer = inputImg.closest('.form__label'),
+			inputImgContainer = inputImg.closest('.form__label', '.form__upload'),
 			fakeTextUrl = inputImgContainer.find('.form__input'),
 			fileName = inputImgContainer.find('.form__upload-group-btn'),
 			progressWrap = inputImgContainer.find('#progress'),
-      progressBar = inputImgContainer.find('.progress-bar'),
-      wrapContainer = $(container).closest('.workspace__img');
+			progressBar = inputImgContainer.find('.progress-bar'),
+			wrapContainer = $(container).closest('.workspace__img');
 
-   //Инициализируем FileUpload
-        $(inputFile).fileupload({
+	 //Инициализируем FileUpload
+				$(inputFile).fileupload({
 
-            // Папка где располагается PHP скрипт jQuery File Upload 
-            url: 'api/upload.php',
+						// Папка где располагается PHP скрипт jQuery File Upload 
+						url: 'api/upload.php',
 
-            // Функция, выполняющаяся при отправке данных на сервер
-            add: function(e, data) {
-                data.submit();
-}
+						// Функция, выполняющаяся при отправке данных на сервер
+						add: function (e, data) {
+							console.log('добавляем');
+								data.submit();
+								},
+						//сервер === "OK"
+						success: function (data) {
+							var imgObj = $.parseJSON(data),
+									imgUrl = 'api/' + imgObj.url;
+
+						 //запись пути элемента в src
+						 $(container).remove;
+						 //если главная картинка, добавляем и меняем размеры
+						 //+ блок под водяной знак
+						 if (container == '.workspace__img') {
+						 	$('<img src="' + imgUrl + '">').appendTo(wrapContainer);
+
+								console.log('Выполнено');
+						 }
+			//главная картинка
+		}
 });
-}
+};
 //возврат объекта. публичные методы
 return {
 	init:init
