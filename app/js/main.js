228119b8938f9a33adc69d2a859791c46ebb51da
Alexandra Veyt <alexandra.pasyuta@gmail.com>
2016-01-15 18:17:07 +0300

    slider js - change watermark opacity

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,7 @@
 var watermark = require('./modules/watermark.js');
+var slider = require('./modules/slider.js');
 
 $(document).ready(function(){
   watermark.init();
+  slider.init();
 });
--- app/js/modules/slider.js
+++ app/js/modules/slider.js
@@ -1,7 +1,31 @@
 //private area
+var slider = function() {
 
-//module.exports = {
-//    init: function () {
-//
-//    }
-//};
+	var defaultValue = 0.5;
+
+	function resetOpasity() {
+		$('#workspaceWt').css("opacity", defaultValue);
+	};
+
+	function changeOpasity() {
+	  	var sliderValue = $( "#slider" ).slider( "value" );
+
+		$('#workspaceWt').css("opacity", sliderValue);
+	};
+
+	$('#slider').slider({
+	  	min: 0,
+	  	max: 1.01,
+	  	step: 0.01,
+	  	value: defaultValue,
+	  	create: resetOpasity,
+	  	slide: changeOpasity
+	})
+
+};
+
+module.exports = {
+    init: function () {
+    	slider();
+    }
+};
