733246687c9868835fd72ef420ffd77b0a5fe3db
Alexandra Veyt <alexandra.pasyuta@gmail.com>
2016-01-15 18:45:20 +0300

    add slider js - change watermark opacity

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,9 @@
 var  ruEng = require('./modules/language.js'),
-	 watermark = require('./modules/watermark.js');
+	 watermark = require('./modules/watermark.js'),
+	 slider = require('./modules/slider.js');
 
 $(document).ready(function(){
   ruEng.init();
   watermark.init();
+  slider.init();
 });
