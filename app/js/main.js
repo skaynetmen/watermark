ad170a3a911d19b137393862dff141b5ee219882
Vadim Novash <novash@tut.by>
2016-01-16 15:22:55 +0300

    Модуль fileUpload

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,13 @@
 var ruEng = require('./modules/language.js'),
 		socialSharing = require ('./modules/socialShare.js'),
+		fileUpload = require('./modules/fileUpload.js'),
 	  slider = require('./modules/slider.js'),
 	  watermark = require('./modules/watermark.js');
 
 $(document).ready(function(){
   ruEng.init();
   socialSharing.init();
+  fileUpload.init();
   slider.init();
   watermark.init();
 });
--- app/js/modules/fileUpload.js
+++ app/js/modules/fileUpload.js
@@ -1,61 +1,242 @@
-// ------------ Функция работы кнопок смены языков ----------------
+"use strict";
 
-var lang = function () {
-    var langInit = function () {
-        _setupListners();
-    };
-    var _setupListners = function(){
-        // В зависимости от того, какая кнопка нажата,
-        // запускается функция с параметрами rus или eng
-        $('#en').on('click', function(){
-            $('.sidebar__link').removeClass('active');
-            $('#en').addClass('active');
-            _ajaxChange('eng');
-        });
-        $('#ru').on('click', function(){
-            $('#en').removeClass('active');
-            $('#ru').addClass('active');
-            _ajaxChange('rus');
-        });
-    };
+var
+    imagePath,
+    watermarkPath,
+    $uploadImg = $('#uploadImg'),
+    $uploadWatermark = $('#uploadWatermark'),
+    $fakeInputImg = $uploadImg.closest('.form__upload').find('.form__input'),
+    $buttonUploadImg = $uploadImg.closest('.form__upload').find('.form__btn'),
+    $fakeInputWatermark = $uploadWatermark.closest('.form__upload').find('.form__input'),
+    $buttonUploadWatermark = $uploadWatermark.closest('.form__upload').find('.form__btn'),
+    imgWidth,
+    imgHeight,
+    watermarkWidth,
+    watermarkHeight,
+    factor = 1,
+    $workspace = $('#workspace'),
+    $workspaceImg = $('.workspace__img'),
+    $workspaceWatermark = $('.workspace__watermark-img'),
+    successUploadImg,
+    successUploadWatermark;
 
-    // Функция смены языков
-    var _ajaxChange = function (lang) {
-        // Делаем AJAX запрос данных на сервер
-        $.ajax({
-                // Адрес обрабатывающего скрипты
-                url: 'api/langChange.php',
-                // Метод, которым отправляем данные
-                type:'POST',
-                // Тип отправляемых данных
-                dataType: 'json',
-                // Сами данные, которые преобразовываем в JSON строку
-                data:'jsonLang=' + JSON.stringify(lang)
-            })
 
-            // При ошибке на сервере, выполняется данная функция
-            .fail(function(langObj) {
-                console.log('Проблемы в PHP');
-            })
+/**
+ * После загрузки исходного изображения задаем высоту обертке и считаем во сколько раз уменьшилось изображение
+ */
+var afterUploadImg = function () {
+    var imgHeightOnWorkspace = $workspaceImg.outerHeight(true);
 
-            // При успешном выполнении скрипта
-            // выводим данные в блоки с текстом
-            .done(function(langObj) {
-                $('.desktop__title').text(langObj.titleContent);
-                $('.settings__title').text(langObj.settings);
-                $('label[for="uploadImg"]').text(langObj.inputMain);
-                $('#fake').text(langObj.inputMainPlace);
-                $('label[for="uploadWatermark"]').text(langObj.inputWater);
-                $('#fakeWat').text(langObj.inputWaterPlace);
-                $('.settings__label').text(langObj.position);
-                $(".settings__label:contains('Прозрачность')").text(langObj.opacity);
-                $('#reset').text(langObj.butClear);
-                $('#submit').text(langObj.butDownload);
-            });
-    };
-    return {
-        init: langInit
+    //если изображение на рабочей области меньше чем на самом деле, считаем во сколько раз оно уменьшилось
+    if (imgHeight > imgHeightOnWorkspace) {
+        factor = imgHeight / imgHeightOnWorkspace;
     }
-}();
 
-lang.init();
+    //для центрирования по вертикали задаем высоту рабочей облости
+    $workspace.css('height', imgHeightOnWorkspace);
+
+    if (typeof successUploadImg == 'function') {
+        successUploadImg.call();
+    }
+};
+
+/**
+ * Загружаем картинку на рабочую область
+ */
+var addImgToWorkspace = function () {
+    var image = new Image();
+
+    image.src = imagePath;
+
+    image.onload = function () {
+        imgWidth = image.width;
+        imgHeight = image.height;
+
+        $workspaceImg.attr('src', imagePath);
+
+        afterUploadImg();
+    };
+};
+
+/**
+ * Колбек загрузки исходного изображения
+ * @param e
+ * @param data
+ */
+var doneImg = function (e, data) {
+    $.each(data.result.files, function (index, file) {
+        if (file.type == 'image/jpeg' || file.type == 'image/png') {
+            imagePath = file.url;
+            addImgToWorkspace();
+        } else {
+            $.ajax({
+                type: file.deleteType,
+                url: file.deleteUrl
+            });
+            alert('Загруженный файл не является изображением!');
+        }
+    });
+};
+
+/**
+ * Колбек прогрессбара
+ * @param e
+ * @param data
+ */
+var progressall = function (e, data) {
+    var progress = parseInt(data.loaded / data.total * 100, 10);
+};
+
+/**
+ * Настройки плагина fileupload для исходного изображения
+ * @type {{url: string, dataType: string, done: doneImg, progressall: progressall}}
+ */
+var uploadImgOptions = {
+    url: '/api/upload.php',
+    dataType: 'json',
+    done: doneImg,
+    progressall: progressall
+};
+
+/**
+ * Масшабируем водяной знак после загрузки
+ */
+var afterUploadWatermark = function () {
+    var height = watermarkHeight / factor;
+
+    $workspaceWatermark.css('height', height);
+
+    if (typeof successUploadWatermark == 'function') {
+        successUploadWatermark.call();
+    }
+};
+
+/**
+ * Загружаем картинку на рабочую область
+ */
+var addWatermarkToWorkspace = function () {
+    var image = new Image();
+
+    image.src = watermarkPath;
+
+    image.onload = function () {
+        watermarkWidth = image.width;
+        watermarkHeight = image.height;
+
+        $workspaceWatermark.attr('src', watermarkPath);
+
+        afterUploadWatermark();
+    };
+};
+
+/**
+ * Колбек загрузки водяного знака
+ * @param e
+ * @param data
+ */
+var doneWatermark = function (e, data) {
+    $.each(data.result.files, function (index, file) {
+        if (file.type == 'image/jpeg' || file.type == 'image/png') {
+            watermarkPath = file.url;
+            addWatermarkToWorkspace();
+        } else {
+            $.ajax({
+                type: file.deleteType,
+                url: file.deleteUrl
+            });
+            alert('Загруженный файл не является изображением!');
+        }
+    });
+};
+
+/**
+ * Настройки плагина fileupload для водяного знака
+ * @type {{url: string, dataType: string, done: doneWatermark, progressall: progressall}}
+ */
+var uploadWatermarkOptions = {
+    url: '/api/upload.php',
+    dataType: 'json',
+    done: doneWatermark,
+    progressall: progressall
+};
+
+/**
+ * Инициализация плагина на поля загрузки
+ */
+var init = function () {
+    $uploadImg.fileupload(uploadImgOptions);
+    $uploadWatermark.fileupload(uploadWatermarkOptions);
+};
+
+/**
+ * Заставляем на нажатию на поле и кнопку открывать выбор файла
+ * и выводим имя файла в фейковое поле
+ */
+var fakeFileInput = function () {
+    var triggerImg = function () {
+        $uploadImg.trigger('click');
+    };
+
+    $fakeInputImg.on('click', triggerImg);
+    $buttonUploadImg.on('click', triggerImg);
+
+    var updateValueImg = function () {
+        $fakeInputImg.val($uploadImg.val().replace("C:\\fakepath\\", ""));
+    };
+
+    $uploadImg.on('change', updateValueImg);
+
+    //------------ watermark -------------
+    var triggerWatermark = function () {
+        $uploadWatermark.trigger('click');
+    };
+
+    $fakeInputWatermark.on('click', triggerWatermark);
+    $buttonUploadWatermark.on('click', triggerWatermark);
+
+    var updateValueWatermark = function () {
+        $fakeInputWatermark.val($uploadWatermark.val().replace("C:\\fakepath\\", ""));
+    };
+
+    $uploadWatermark.on('change', updateValueWatermark);
+};
+
+
+
+module.exports = {
+    init: function () {
+        fakeFileInput();
+        init();
+    },
+    getImgPath: function () {
+        return imagePath;
+    },
+    getWatermarkPath: function () {
+        return watermarkPath;
+    },
+    getImgWidth: function () {
+        return imgWidth;
+    },
+    getImgHeight: function () {
+        return imgHeight;
+    },
+    getWatermarkWidth: function () {
+        return watermarkWidth;
+    },
+    getWatermarkHeight: function () {
+        return watermarkHeight;
+    },
+    getFactor: function () {
+        return factor;
+    },
+    setSuccessUploadImg: function (callback) {
+        if (typeof callback == 'function') {
+            successUploadImg = callback;
+        }
+    },
+    setSuccessUploadWatermark: function (callback) {
+        if (typeof callback == 'function') {
+            successUploadWatermark = callback;
+        }
+    }
+};
