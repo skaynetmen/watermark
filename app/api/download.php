25edf800ec45977f47d86e200a52e19d90c12a78
zoryna-petrova <zoryana.petrova@gmail.com>
2016-01-16 15:01:56 +0200

    Added validation for input params

--- app/api/download.php
+++ app/api/download.php
@@ -5,122 +5,105 @@ define('DOWNLOADS_PATH', '/downloads/');
 
 require_once __DIR__.'/../../vendor/autoload.php';
 
+
+function post($key, $isRequired = true)
+{
+    if (array_key_exists($key, $_POST)) {
+        return $_POST[$key];
+    } elseif ($isRequired) {
+        throw new \InvalidArgumentException("Input parameter $key is not set");
+    }
+
+    return null;
+}
+
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-    // путь к картинке
-    $imagePath = $_POST['imagePath'];
-
-    // путь к вотермарку
-    $watermarkPath = $_POST['watermarkPath'];
-
-    // Во сколько раз были уменьшены изборажения
-    $scaleFactor = $_POST['scaleFactor'];
-
-    // позиция watermark по оси X
-    $watermarkPosX = $_POST['watermarkPosX'];
-
-    // позиция watermark по оси У
-    $watermarkPosY = $_POST['watermarkPosY'];
-
-    // прозрачность watermark
-    $watermarkOpacity = $_POST['watermarkOpacity'];
-
-    // необходимо ли повторять вотермарк
-    $watermarkRepeat = $_POST['watermarkRepeat'];
-
-    // Количество вотермарков по оси X
-    $watermarkRepeatXNumber = $_POST['watermarkRepeatXNumber'];
-
-    // Количество вотермарков по оси X
-    $watermarkRepeatYNumber = $_POST['watermarkRepeatYNumber'];
-
-    // Отступ у вотермарка слева
-    $watermarkMarginLeft = $_POST['watermarkMarginLeft'];
-
-    // Отступ у вотермарка снизу
-    $watermarkMarginBottom = $_POST['watermarkMarginBottom'];
-
-    // Создаём класс imagine
-    $imagine = new Imagine\Imagick\Imagine();
-
-    // Открываем основную картинку
     try {
-        $image = $imagine->open(__DIR__.'/..'.$imagePath);
+        // путь к картинке
+        $imagePath = post('imagePath');
 
-    } catch (\Exception $e) {
-        echo json_encode([
-            'error' => true,
-            'errorMsg' => $e->getMessage(),
-            'errorTrace' => $e->getTrace()
-        ]);
-        die;
-    }
+        // путь к вотермарку
+        $watermarkPath = post('watermarkPath');
 
-    $imageSize = $image->getSize();
+        // Во сколько раз были уменьшены изборажения
+        $scaleFactor = (float) post('scaleFactor');
 
-    // Открываем картинку Watermark
-    try {
-        $watermarkImage = $imagine->open(__DIR__.'/..'.$watermarkPath);
+        // позиция watermark по оси X
+        $watermarkPosX = (int) post('watermarkPosX');
 
-    } catch (\Exception $e) {
-        echo json_encode([
-            'error' => true,
-            'errorMsg' => $e->getMessage(),
-            'errorTrace' => $e->getTrace()
-        ]);
-        die;
-    }
+        // позиция watermark по оси У
+        $watermarkPosY = (int) post('watermarkPosY');
 
+        // прозрачность watermark
+        $watermarkOpacity = (float) post('watermarkOpacity');
 
-    // Устанавливаем уровень прозрачности
-    $watermarkImage->getImagick()->setImageOpacity($watermarkOpacity);
+        // необходимо ли повторять вотермарк
+        $watermarkRepeat = (bool) post('watermarkRepeat');
 
-    // Размер вотермарка
-    $watermarkSize = $watermarkImage->getSize();
+        if ($watermarkRepeat === true) {
 
+            // Количество вотермарков по оси X
+            $watermarkRepeatXNumber = (int) post('watermarkRepeatXNumber');
 
-    // Если необходимо замощение вотермарка
-    if ($watermarkRepeat) {
-        // Считаем длину и высоту холста для вотермарков
-        $watermarkLayerWidth = ($watermarkMarginLeft + $watermarkSize->getWidth()) * $watermarkRepeatXNumber;
-        $watermarkLayerHeight = ($watermarkMarginBottom + $watermarkSize->getHeight()) * $watermarkRepeatYNumber;
+            // Количество вотермарков по оси X
+            $watermarkRepeatYNumber = (int) post('watermarkRepeatYNumber');
 
-        // Создаём холст из вотермарков
-        $palette = new Imagine\Image\Palette\RGB();
-        $color = $palette->color('#fff', 0);
-        $boundingBox = new \Imagine\Image\Box($watermarkLayerWidth * $scaleFactor, $watermarkLayerHeight * $scaleFactor);
-        $watermarkImageLayer = $imagine->create($boundingBox, $color);
+            // Отступ у вотермарка слева
+            $watermarkMarginLeft = (int) post('watermarkMarginLeft');
 
-        // Заполняем холст вотермарками
-        for ($i = 0; $i < $watermarkRepeatXNumber; $i++) {
-            for ($j = 0; $j < $watermarkRepeatYNumber; $j++) {
-                $posX = ($watermarkMarginLeft + $watermarkSize->getWidth()) * $i;
-                $posY = ($watermarkMarginBottom + $watermarkSize->getHeight()) * $j;
-
-                $watermarkPosition = new Imagine\Image\Point($posX * $scaleFactor, $posY * $scaleFactor);
-
-                try {
-                    $watermarkImageLayer->paste($watermarkImage, $watermarkPosition);
-                } catch (\Exception $e) {
-                    echo json_encode([
-                        'error' => true,
-                        'errorMsg' => $e->getMessage(),
-                        'errorTrace' => $e->getTrace()
-                    ]);
-                    die;
-                }
-
-            }
+            // Отступ у вотермарка снизу
+            $watermarkMarginBottom = (int) post('watermarkMarginBottom');
         }
 
-    } else {
-        $watermarkImageLayer = $watermarkImage;
-    }
+        // Создаём класс imagine
+        $imagine = new Imagine\Imagick\Imagine();
 
-    // Позиция на основной картинке
-    $watermarkPosition = new Imagine\Image\Point($watermarkPosX * $scaleFactor, $watermarkPosY * $scaleFactor);
+        // Открываем основную картинку
+        $image = $imagine->open(__DIR__.'/..'.$imagePath);
 
-    try {
+        $imageSize = $image->getSize();
+
+        // Открываем картинку Watermark
+        $watermarkImage = $imagine->open(__DIR__.'/..'.$watermarkPath);
+
+        // Устанавливаем уровень прозрачности
+        $watermarkImage->getImagick()->setImageOpacity($watermarkOpacity);
+
+        // Размер вотермарка
+        $watermarkSize = $watermarkImage->getSize();
+
+        // Если необходимо замощение вотермарка
+        if ($watermarkRepeat) {
+            // Считаем длину и высоту холста для вотермарков
+            $watermarkLayerWidth = ($watermarkMarginLeft + $watermarkSize->getWidth()) * $watermarkRepeatXNumber;
+            $watermarkLayerHeight = ($watermarkMarginBottom + $watermarkSize->getHeight()) * $watermarkRepeatYNumber;
+
+            // Создаём холст из вотермарков
+            $palette = new Imagine\Image\Palette\RGB();
+            $color = $palette->color('#fff', 0);
+            $boundingBox = new \Imagine\Image\Box($watermarkLayerWidth * $scaleFactor, $watermarkLayerHeight * $scaleFactor);
+            $watermarkImageLayer = $imagine->create($boundingBox, $color);
+
+            // Заполняем холст вотермарками
+            for ($i = 0; $i < $watermarkRepeatXNumber; $i++) {
+                for ($j = 0; $j < $watermarkRepeatYNumber; $j++) {
+                    $posX = ($watermarkMarginLeft + $watermarkSize->getWidth()) * $i;
+                    $posY = ($watermarkMarginBottom + $watermarkSize->getHeight()) * $j;
+
+                    $watermarkPosition = new Imagine\Image\Point($posX * $scaleFactor, $posY * $scaleFactor);
+
+                    $watermarkImageLayer->paste($watermarkImage, $watermarkPosition);
+                }
+            }
+
+        } else {
+            $watermarkImageLayer = $watermarkImage;
+        }
+
+        // Позиция на основной картинке
+        $watermarkPosition = new Imagine\Image\Point($watermarkPosX * $scaleFactor, $watermarkPosY * $scaleFactor);
+
         // Обрезаем слой вотермарка до размеров изображения на которое будет накладывать
         $boundingBox = new \Imagine\Image\Box($imageSize->getWidth() - $watermarkPosition->getX(), $imageSize->getHeight() - $watermarkPosition->getY());
         $watermarkImageLayer->crop(new \Imagine\Image\Point(0, 0), $boundingBox);
@@ -128,21 +111,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Вставляем вотермарк на основную картинку
         $image->paste($watermarkImageLayer, $watermarkPosition);
 
-    } catch (\Exception $e) {
-        echo json_encode([
-            'error' => true,
-            'errorMsg' => $e->getMessage(),
-            'errorTrace' => $e->getTrace()
-        ]);
-        die;
-    }
 
+        // Сохранить изображение
+        $imageFileName = uniqid().'.png';
+        $imagePath = DOWNLOADS_DIR.'/'.$imageFileName;
 
-    // Сохранить изображение
-    $imageFileName = uniqid().'.png';
-    $imagePath = DOWNLOADS_DIR.'/'.$imageFileName;
-
-    try {
         $image->save($imagePath);
 
         echo json_encode([
@@ -156,6 +129,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             'errorMsg' => $e->getMessage(),
             'errorTrace' => $e->getTrace()
         ]);
-        die;
     }
 }
